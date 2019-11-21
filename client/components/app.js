82e04d0a0da213db4d1c7dedfb769f6f2fa2eec7
Abhinav <singh.abhinav6996@gmail.com>
2019-11-21 15:39:12 +0530

    Update component structure

--- client/components/app.js
+++ client/components/app.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
-import Home from "./Home";
 import "../scss/app.scss";
+import Header from "./Header";
+import Login from "./Login";
 
 export class app extends Component {
   render() {
     return (
       <>
-        <Home />
+        <Header />
+        <Login />
       </>
     );
   }
