8e7a0879b3c5d3b8a60aa5d7d3c5505d459b1b27
prarora98 <prateekisthebest98@gmail.com>
2019-11-22 15:43:43 +0530

    Backend-setup done

--- models/User.js
+++ models/User.js
@@ -24,6 +24,8 @@ userSchema.pre("save", async function(next) {
   }
   next();
 });
+
+
 const User = mongoose.model("User", userSchema);
 
 module.exports = User;
--- routes/user.js
+++ routes/user.js
@@ -2,16 +2,22 @@ var express = require("express");
 var router = express.Router();
 
 // on first login reset Password
-router.post("/set-password/:hashMail", async (req, res) => {
+router.post("/:hashMail", async (req, res) => {
   let { password } = req.body;
-  try {
-    const user = await User.findOne({ hashMail });
-    user.password = password;
-    const UpdatedUser = await user.save();
-    res.status(201).json({ status: true, UpdatedUser });
-  } catch (err) {
-    res.status(301).json({ success: false, err });
+  let {hashMail} = req.params;
+  if(hashMail){
+    try {
+      const user = await User.findOne({ hashMail });
+      user.password = password;
+      delete user.hashMail;
+      const UpdatedUser = await user.save();
+      return res.status(201).json({ status: true, UpdatedUser });
+    } catch (err) {
+      return res.status(301).json({ success: false, err });
+    }
   }
+  else{
+  res.status(301).json({success:false,message:'User already Claimed there account'});}
 });
 
 module.exports = router;
