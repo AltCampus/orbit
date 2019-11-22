579806dd81a2e4ae12e177f39502fbdb15923649
prarora98 <prateekisthebest98@gmail.com>
2019-11-22 15:54:56 +0530

    add: validate email in user schema

--- models/User.js
+++ models/User.js
@@ -1,10 +1,25 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 const Schema = mongoose.Schema;
+
+// function validate email
+var validateEmail = function(email) {
+  var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return re.test(email)
+};
+
 const userSchema = new Schema(
   {
     name: { type: String, required: true },
-    email: { type: String, required: true, unique: false, lowercase: true },
+    email: {
+      type: String,
+      trim: true,
+      lowercase: true,
+      unique: true,
+      required: 'Email address is required',
+      validate: [validateEmail, 'Please fill a valid email address'],
+      match: [/^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/, 'Please fill a valid email address']
+  },
     password: String,
     phoneNo: {
       type: Number,
