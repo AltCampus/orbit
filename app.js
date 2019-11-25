f5b3d8d65f38e749b18c4fa9254484751ea7d20e
Ckbhatia <chetansain86@gmail.com>
2019-11-25 11:05:10 +0530

    fix: replace the jade view engine with ejs

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ mongoose.set("useCreateIndex", true);
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
+app.set("view engine", "ejs");
 
 app.use(logger("dev"));
 app.use(express.json());
