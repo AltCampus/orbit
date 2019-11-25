85c8cd7bce805cbdfa26f8669aaebd7e8e86b6a3
Ckbhatia <chetansain86@gmail.com>
2019-11-25 22:08:48 +0530

    feat: add post user password functionality

--- client/components/resetForm/ResetForm.js
+++ client/components/resetForm/ResetForm.js
@@ -1,14 +1,33 @@
 import React from "react";
 import "../login/Login";
+import axios from "axios";
 export default class ResetForm extends React.Component {
   state = {
     password: ""
   };
+
   handleSubmit = e => {
     e.preventDefault();
+    // Extracting the hasmail
+    const hashMail = this.props.match.params.hashmail;
+    // Invoke the fetchPassword with hashMail
+    this.fetchResetPassword(hashMail);
   };
+
   handleChange = e => {
     this.setState({ [e.target.name]: e.target.value });
+  };
+
+  fetchResetPassword = hashmail => {
+    let password = { password: this.state.password };
+    password = JSON.stringify(password);
+    // Post the user password
+    axios
+      .post(`http://localhost:3000/user/${hashmail}`, password)
+      .then(res =>
+        res.status ? this.props.history.push("/login") : console.error(res)
+      )
+      .catch(err => console.error(err));
   };
 
   render() {
