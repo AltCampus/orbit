7a9a2e4f076686112a6bd5ec7787a205064c12eb
prateekcodes <prateekisthebest98@gmail.com>
2019-12-02 16:18:56 +0530

    add: question schema

---
+++ models/Question.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const questionSchema = new Schema({
+  question: { type: String, required: true },
+  type: { type: String, required: true },
+  options: {
+    A: { type: String },
+    B: { type: String },
+    C: { type: String },
+    D: { type: String }
+  },
+  answer: { type: String },
+  point: { type: Number },
+  isActive: { type: Boolean, default: false }
+});
+
+const Question = mongoose.model("Question", questionSchema);
+
+module.exports = Question;
