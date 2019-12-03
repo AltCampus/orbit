593d355be10e557ecd92e39e431049eab907c63b
Ckbhatia <chetansain86@gmail.com>
2019-12-03 23:05:14 +0530

    feat: connect login to redux

--- client/components/login/Login.js
+++ client/components/login/Login.js
@@ -2,6 +2,8 @@ import React from "react";
 import { withRouter } from "react-router-dom";
 import axios from "axios";
 import { message } from "antd";
+import { connect } from "react-redux";
+import { userLogin } from "../../actions/users";
 import "./Login.scss";
 
 class Login extends React.Component {
@@ -12,64 +14,67 @@ class Login extends React.Component {
 
   handleSubmit = e => {
     e.preventDefault();
-    this.userLogin();
+    this.props.userLogin(this.state);
+    // TODO: Redirect the user after request success
+    this.props.history.push("/dashboard");
   };
 
   handleChange = e => {
     this.setState({ [e.target.name]: e.target.value });
   };
 
-  userLogin = async () => {
-    try {
-      if (!this.state.email || !this.state.password) {
-        message.error("Please Fill Both Fields");
-      } else {
-        // Post the user data
-        const userLogin = await axios.post(
-          `http://localhost:3000/api/v1/users/login`,
-          this.state
-        );
-        let { data } = userLogin;
-        if (!data.status) {
-          message.error(data.message);
-          console.error(data.message);
-        } else {
-          localStorage.setItem("authToken", JSON.stringify(data.authToken));
-          this.props.verifyToken(data.authToken);
-        }
-      }
-    } catch (error) {
-      message.warning(error);
-      this.props.history.push("/login");
-    }
-  };
+  // userLogin = async () => {
+  //   try {
+  //     if (!this.state.email || !this.state.password) {
+  //       message.error("Please Fill Both Fields");
+  //     } else {
+  //       // Post the user data
+  //       const userLogin = await axios.post(
+  //         `http://localhost:3000/api/v1/users/login`,
+  //         this.state
+  //       );
+  //       let { data } = userLogin;
+  //       if (!data.status) {
+  //         message.error(data.message);
+  //         console.error(data.message);
+  //       } else {
+  //         localStorage.setItem("authToken", JSON.stringify(data.authToken));
+  //         // this.props.verifyToken(data.authToken);
+  //       }
+  //     }
+  //   } catch (error) {
+  //     message.warning(error);
+  //     this.props.history.push("/login");
+  //   }
+  // };
 
   render() {
     return (
-      <div className='login-container'>
-        <div className='login-content'>
-          <div className='login-header'>
-            <h1 className='login-title'>Sign In</h1>
+      <div className="login-container">
+        <div className="login-content">
+          <div className="login-header">
+            <h1 className="login-title">Sign In</h1>
           </div>
-          <form className='login-form' onSubmit={this.handleSubmit}>
+          <form className="login-form" onSubmit={this.handleSubmit}>
             <input
-              type='email'
-              name='email'
-              placeholder='Email'
-              pattern='[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$'
+              type="email"
+              name="email"
+              placeholder="Email"
+              pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
               onChange={this.handleChange}
             />
             <input
-              type='password'
-              name='password'
-              placeholder='Password'
+              type="password"
+              name="password"
+              placeholder="Password"
               onChange={this.handleChange}
             />
-            <button type='submit'>Login</button>
+            <button type="submit">Login</button>
           </form>
         </div>
       </div>
     );
   }
 }
-export default withRouter(Login);
+
+export default withRouter(connect(null, { userLogin })(Login));
