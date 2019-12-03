6214cc01d5489a4a99263ee22de9366c15d4d914
Ckbhatia <chetansain86@gmail.com>
2019-12-03 23:04:10 +0530

    feat: add get user success reducer and changes

--- client/reducers/currentUser.js
+++ client/reducers/currentUser.js
@@ -1,4 +1,10 @@
-import { USER_LOGIN_SUCCESS, LOG_OUT, NO_TOKEN } from "./../actions/types";
+import {
+  USER_LOGIN_SUCCESS,
+  GET_USER_SUCCESS,
+  LOG_OUT,
+  NO_TOKEN,
+  UPDATE_TOKEN
+} from "./../actions/types";
 
 const initialState = {
   user: null,
@@ -12,10 +18,16 @@ const currentUser = (state = initialState, action) => {
     case USER_LOGIN_SUCCESS:
       return {
         ...state,
-        user: action.data.user,
+        token: action.data,
         isAuthenticated: true,
         isAuthInProgress: false
       };
+    case GET_USER_SUCCESS: {
+      return {
+        ...state,
+        user: action.data
+      };
+    }
     case LOG_OUT:
     case NO_TOKEN:
       return {
