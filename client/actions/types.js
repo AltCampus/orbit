f1cc711453c22f47364a9222b9ff557a89f00251
Ckbhatia <chetansain86@gmail.com>
2019-12-03 22:58:09 +0530

    feat: add two new types

--- client/actions/types.js
+++ client/actions/types.js
@@ -1,3 +1,5 @@
 export const USER_LOGIN_SUCCESS = "USER_LOGIN_SUCCESS";
 export const NO_TOKEN = "NO_TOKEN";
 export const LOG_OUT = "LOG_OUT";
+export const UPDATE_TOKEN = "UPDATE_TOKEN";
+export const GET_USER_SUCCESS = "GET_USER_SUCCESS";
