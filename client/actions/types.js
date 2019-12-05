1ad754d3f8306bf52c36a32ea25666de8cdda08b
Ckbhatia <chetansain86@gmail.com>
2019-12-05 12:53:34 +0530

    feat: add two new types and remove update token

--- client/actions/types.js
+++ client/actions/types.js
@@ -1,5 +1,6 @@
+export const SET_ERROR = "SET_ERROR";
 export const USER_LOGIN_SUCCESS = "USER_LOGIN_SUCCESS";
+export const GET_USER_PENDING = "GET_USER_PENDING";
+export const GET_USER_SUCCESS = "GET_USER_SUCCESS";
 export const NO_TOKEN = "NO_TOKEN";
 export const LOG_OUT = "LOG_OUT";
-export const UPDATE_TOKEN = "UPDATE_TOKEN";
-export const GET_USER_SUCCESS = "GET_USER_SUCCESS";
