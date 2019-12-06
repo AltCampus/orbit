dc7d942f5223cf85c62c6178c21299673ef06037
ckbhatia <chetansain86@gmail.com>
2019-12-06 20:02:52 +0530

    fix: remove bugs

--- client/components/task/taskTwo/TaskTwo.js
+++ client/components/task/taskTwo/TaskTwo.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
+import { connect } from "react-redux";
 import { updateToken, getCurrentUser } from "../../../actions/users";
 
 import { Card, Col, Row, Input, Button, message } from "antd";
