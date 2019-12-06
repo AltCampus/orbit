67f5b6d2aa210073439af9b448c2a71a53fafec8
Sunny <sunnysinghrajavatssr@gmail.com>
2019-12-06 11:22:57 +0530

    fix: update user routes

--- routes/users.js
+++ routes/users.js
@@ -20,20 +20,29 @@ router.post("/", async (req, res) => {
   try {
     // validations for required profile
     if (!name || !email || !phoneNo || !socialProfile || !motivation) {
-      res.json({ status: false, message: "Please fill all Required data" });
+      return res.status(400).json({
+        status: false,
+        message: "Please fill all Required data"
+      });
     }
     req.body.hashMail =
       Date.now() +
       Math.random()
         .toString(36)
         .substring(2, 15);
-    const user = await User.create(req.body);
+    const user = await User.create({
+      name,
+      email,
+      phoneNo,
+      socialProfile,
+      motivation
+    });
     //TODO: UnComment to sending mail once user Register
     // const mail = await Mailer.mail(user.email, user.name, user.hashMail);
     // console.log("mailer");
-    res.status(201).json({ status: true, user });
+    return res.status(201).json({ status: true, user });
   } catch (error) {
-    res.status(400).json({ status: false, error });
+    return res.status(400).json({ status: false, error });
   }
 });
 
@@ -43,23 +52,29 @@ router.post("/login", async (req, res) => {
     let { email, password } = req.body;
     // validations for required profile
     if (!email || !password) {
-      res.json({ status: false, message: "Please Fill Both Fields" });
+      return res
+        .status(400)
+        .json({ status: false, message: "Please Fill Both Fields" });
     }
     const user = await User.findOne({ email: req.body.email });
 
     if (!user) {
-      res.json({ status: false, message: "User not found!!!" });
+      return res
+        .status(400)
+        .json({ status: false, message: "User not found!!!" });
     }
 
     if (!user.verifyPassword(password)) {
-      res.json({ status: false, message: "Invaild password" });
+      return res
+        .status(400)
+        .json({ status: false, message: "Invaild password" });
     }
 
     const authToken = await auth.generateToken(user.id);
     console.log("done");
-    res.status(200).json({ status: "success", authToken });
+    return res.status(200).json({ status: "success", authToken });
   } catch (error) {
-    res.status(400).json({ status: "failed", error });
+    return res.status(400).json({ status: "failed", error });
   }
 });
 
@@ -87,15 +102,15 @@ router.post("/:hashMail", async (req, res) => {
       // Set user profile to be claimed.
       user.isProfileClaimed = true;
       const updatedUser = await user.save();
-      res.status(201).json({ status: true, user: updatedUser });
+      return res.status(201).json({ status: true, user: updatedUser });
     } else {
-      res.status(301).json({
+      return res.status(301).json({
         success: false,
         message: "User already Claimed there account"
       });
     }
   } catch (error) {
-    res.status(301).json({ success: false, error });
+    return res.status(301).json({ success: false, error });
   }
 });
 
