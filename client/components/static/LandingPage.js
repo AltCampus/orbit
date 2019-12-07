5165cf4d992cd4e2546ddc08c63dd43918fd201b
Abhinav <singh.abhinav6996@gmail.com>
2019-12-07 12:48:34 +0530

    add:Landing page

--- client/components/static/LandingPage.js
+++ client/components/static/LandingPage.js
@@ -1,13 +1,33 @@
-import React from 'react';
-import { Link } from 'react-router-dom'; 
+import React from "react";
+import { Link } from "react-router-dom";
+import { Row, Col, Button } from "antd";
 
 const LandingPage = () => {
   return (
-    <div>
-      <div>Hello World! I am Orbit.</div>
-      <Link to="/login">Login</Link>
-    </div>
-  )
-}
+    <>
+      <div className="banner-container wrapper">
+        <Row>
+          <Col span={15}>
+            <img
+              className="image-wrapper"
+              src="https://i.ibb.co/f9mjDPC/banner.png"
+              alt="banner"
+              border="0"
+            />
+          </Col>
+          <Col span={7} className="content-container">
+            <div>
+              <strong>Welcome</strong>
+              <p>to Orbit</p>
+            </div>
+            <Link to="/login">
+              <Button type="primary">Login</Button>
+            </Link>
+          </Col>
+        </Row>
+      </div>
+    </>
+  );
+};
 
 export default LandingPage;
