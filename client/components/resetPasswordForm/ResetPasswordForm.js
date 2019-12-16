1c80ec45d970117723fc012653de034a34410e60
prateekcodes <prateekisthebest98@gmail.com>
2019-12-16 15:37:22 +0530

    fix: error handle into ResetPassoword component

--- client/components/resetPasswordForm/ResetPasswordForm.js
+++ client/components/resetPasswordForm/ResetPasswordForm.js
@@ -1,6 +1,8 @@
 import React from "react";
-import "../login/Login";
 import axios from "axios";
+import { message } from "antd";
+
+import "../login/Login";
 export default class ResetForm extends React.Component {
   state = {
     password: ""
@@ -8,43 +10,50 @@ export default class ResetForm extends React.Component {
 
   handleSubmit = e => {
     e.preventDefault();
-    // Extracting the hasmail
-    const hashMail = this.props.match.params.hashmail;
-    // Invoke the fetchPassword with hashMail
-    this.fetchResetPassword(hashMail);
+    if (this.state.password.length < 6) {
+      message.warning("Password must contain 6 letter.");
+    } else {
+      // Extracting the hasmail
+      const hashMail = this.props.match.params.hashmail;
+      // Invoke the fetchPassword with hashMail
+      this.fetchResetPassword(hashMail);
+    }
   };
 
   handleChange = e => {
     this.setState({ [e.target.name]: e.target.value });
   };
 
-  fetchResetPassword = hashmail => {
-    let password = { password: this.state.password };
-    password = JSON.stringify(password);
-    // Post the user password
-    axios
-      .post(`http://localhost:3000/api/v1/users/${hashmail}`, password)
-      .then(res =>
-        res.status ? this.props.history.push("/login") : console.error(res)
-      )
-      .catch(err => console.error(err));
+  fetchResetPassword = async hashmail => {
+    try {
+      let password = { password: this.state.password };
+      // Post the user password
+      const res = await axios.post(
+        `http://localhost:3000/api/v1/users/${hashmail}`,
+        password
+      );
+      message.success("PassWord Reset Now you Can Login!");
+    } catch (error) {
+      message.error(error.data.message);
+      console.log(error);
+    }
   };
 
   render() {
     return (
-      <div className='login-container'>
-        <div className='login-content'>
-          <div className='login-header'>
-            <h1 className='login-title'>Set password for your account</h1>
+      <div className="login-container">
+        <div className="login-content">
+          <div className="login-header">
+            <h1 className="login-title">Set password for your account</h1>
           </div>
-          <form className='login-form' onSubmit={this.handleSubmit}>
+          <form className="login-form" onSubmit={this.handleSubmit}>
             <input
-              type='password'
-              name='password'
-              placeholder='Password'
+              type="password"
+              name="password"
+              placeholder="Password"
               onChange={this.handleChange}
             />
-            <button type='submit'>Set</button>
+            <button type="submit">Set</button>
           </form>
         </div>
       </div>
