4bf7200bbe20adcfaee6e450f6d93e84e22d295c
ckbhatia <chetansain86@gmail.com>
2019-12-19 20:23:56 +0530

    feat: add a new case use login pending

--- client/reducers/currentUser.js
+++ client/reducers/currentUser.js
@@ -5,7 +5,8 @@ import {
   LOG_OUT,
   NO_TOKEN,
   SET_ERROR,
-  USER_STAGE_UPGRADE
+  USER_STAGE_UPGRADE,
+  USER_LOGIN_PENDING
 } from "./../actions/types";
 
 const initialState = {
@@ -13,6 +14,7 @@ const initialState = {
   token: null,
   isAuthenticated: false,
   isAuthInProgress: false,
+  isLoginInProgress: false,
   isError: false
 };
 
@@ -24,12 +26,19 @@ const currentUser = (state = initialState, action) => {
         isError: true
       };
     }
+    case USER_LOGIN_PENDING: {
+      return {
+        ...state,
+        isLoginInProgress: true
+      };
+    }
     case USER_LOGIN_SUCCESS: {
       return {
         ...state,
         token: action.data,
         isAuthenticated: true,
-        isAuthInProgress: false
+        isAuthInProgress: false,
+        isLoginInProgress: false
       };
     }
     case GET_USER_PENDING: {
