e0c7abe6615ce93e360cc8c3af69611a4ddb1cbb
aman <amanregu@gmail.com>
2019-12-20 10:55:30 +0530

    fix katas-solved

--- routes/tasks.js
+++ routes/tasks.js
@@ -240,7 +240,7 @@ router.post("/two/save", auth.verifyToken, (req, res) => {
         if (apiResponse.statusCode === 200) {
           // Username is valid
           // const endTime = new Date(Date.now() + 259200 * 1000);
-          const endTime = new Date(Date.now() + 30000);
+          const endTime = new Date(Date.now() + 500000);
           console.log(endTime);
           const codewarsTask = {
             codewarsUsername: username,
@@ -302,13 +302,12 @@ router.post('/two/katas', auth.verifyAdminToken, (req, res) => {
         `https://www.codewars.com/api/v1/users/${codewars.codewarsUsername}/code-challenges/completed?page=0`
       );
       // console.log(response.data.data);
-      const currentDate = new Date().toISOString();
       const filteredArray = response.data.data.filter(
-        kata => kata.completedAt < currentDate
+        kata => kata.completedAt > codewars.startTime
       );
       console.log(filteredArray, 'FIL111');
       const refilteredArray = filteredArray.filter(
-        kata => kata.completedAt > codewars.submitTime
+        kata => kata.completedAt < codewars.endTime
       );
       console.log(refilteredArray, 'FIL222');
       const katasSolved = refilteredArray.length;
