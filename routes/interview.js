affd6f03de90568ff203d0aaf641dd84cb6d8efd
prateekcodes <prateekisthebest98@gmail.com>
2019-12-23 20:48:56 +0530

    update: Interview route

--- routes/interview.js
+++ routes/interview.js
@@ -1,30 +1,58 @@
 const express = require("express");
 const Router = express.Router();
 
+const auth = require("./../utils/auth");
+const user = require("./../models/User");
 const Interview = require("../models/Interview");
-const User = require("../models/User");
 
-// get data from calendly
-Router.post("/", async (req, res) => {
+Router.get("/", auth.verifyToken, async (req, res) => {
+  const data = await Interview.find({
+    user: null,
+    date: { $gt: Date.now() }
+  });
+  res.json({ data });
+});
+
+//Admin create Interview
+Router.post("/", auth.verifyAdminToken, async (req, res) => {
+  const { date, startTime, endTime } = req.body;
   try {
-    const { uuid, start_time, end_time } = req.body.event;
-    const scheduleEvent = { uuid, start_time, end_time };
-    const { email } = req.body.invitee;
-    const user = await User.findOne({ email });
-    if (user.canScheduleInterview) {
-      const interview = await Interview.create({
-        user: user._id,
-        scheduleEvent
+    if (!date || !startTime || !endTime) {
+      res
+        .status(401)
+        .json({ status: false, message: "Time and date are must required." });
+    } else {
+      const interviewSchedule = await Interview.create({ dateAndTime });
+      res.status(201).json({
+        sucess: true,
+        message: "Interview created"
       });
-      user.interview = interview.id;
-      await user.save();
     }
-    res.end();
   } catch (error) {
-    res.status(400).json({
-      status: false,
-      message: "error in Interview Schedule Interview"
+    console.log(error);
+    res.status(400).json({ status: false, error });
+  }
+});
+
+Router.put("/:id", auth.verifyToken, async (req, res) => {
+  try {
+    const { id } = req.params.id;
+    const ScheduledInterview = await Interview.findByIdAndUpdate(
+      id,
+      { user: req.user._id },
+      { new: true }
+    );
+    await user.findOneAndUpdate(
+      { _id: req.user._id },
+      { interview: ScheduledInterview._id }
+    );
+    res.status(201).json({
+      status: true,
+      message: "Interview Scheduled"
     });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ status: false, error });
   }
 });
 
