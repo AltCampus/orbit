fcaa9091231fd5f6c7c05d6a5ff6f9c4ce86f588
Sunny <sunnysinghrajavatssr@gmail.com>
2020-01-10 14:19:09 +0530

    New: Add 404Page

--- client/components/message/Error.js
+++ client/components/message/Error.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Result, Button } from "antd";
+import { Link } from "react-router-dom";
 
 export default function Error() {
   return (
     <>
       <Result
-        status='404'
-        title='404'
-        subTitle='Sorry, the page you visited does not exist.'
+        status="404"
+        title="404"
+        subTitle="Sorry, the page you visited does not exist."
         extra={
-          <Link to='/'>
-            <Button type='primary'>Back Home</Button>
+          <Link to="/">
+            <Button type="primary">Go Back To Home</Button>
           </Link>
         }
       />
