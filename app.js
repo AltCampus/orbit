c04f70a16eb5c2c9cee5323e0639fad2c5c7e632
Prashant Abhishek <prashant.abhishek7g@gmail.com>
2020-02-03 11:25:21 +0530

    add changes to app

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 // app.use("/", express.static(path.join(__dirname, "dist")));
-app.use("/", expressStaticGzip(path.join(__dirname, "dist"), gzipOptions)
+app.use("/", expressStaticGzip(path.join(__dirname, "dist"), gzipOptions));
 
 if (process.env.NODE_ENV === "development") {
   const webpack = require("webpack");
