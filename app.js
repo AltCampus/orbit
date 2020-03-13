759050990cf4438db21c03e8116f42030016bc87
Prashant Abhishek <prashant.abhishek7g@gmail.com>
2020-03-13 16:19:06 +0530

    explicit line for docs

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
-app.use("/docs", express.static(path.join(__dirname, "public")));
+app.use("/docs", express.static(path.join(__dirname, "public/docs")));
 // app.use("/", express.static(path.join(__dirname, "dist")));
 app.use("/bundle", expressStaticGzip(path.join(__dirname, "dist/bundle"), gzipOptions));
 
